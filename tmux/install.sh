d36e0c637a164e882849baa03f6b551dd2dfc02d
Aleksey V Zapparov <ixti@member.fsf.org>
2013-06-14 00:11:42 +0200

    Timestampify backups after install

--- tmux/install.sh
+++ tmux/install.sh
@@ -10,3 +10,6 @@ test -e ~/.tmux.conf  && mkdir -p backup && cp -r ~/.tmux.conf  backup
 
 rm -rf ~/.tmux      && ln -s $PWD/.tmux      ~/.tmux
 rm -rf ~/.tmux.conf && ln -s $PWD/.tmux.conf ~/.tmux.conf
+
+
+test -e backup && mv backup backup-$(date +%s)
--- vim/install.sh
+++ vim/install.sh
@@ -10,3 +10,6 @@ test -e ~/.vimrc  && mkdir -p backup && cp -r ~/.vimrc  backup
 
 rm -rf ~/.vim   && ln -s $PWD/.vim   ~/.vim
 rm -rf ~/.vimrc && ln -s $PWD/.vimrc ~/.vimrc
+
+
+test -e backup && mv backup backup-$(date +%s)

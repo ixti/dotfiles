3c3be2ce999d42556e5ebea0d7c8b6c22c77255c
Aleksey V Zapparov <ixti@member.fsf.org>
2013-06-17 19:25:36 +0200

    Install powerline config

--- vim/install.sh
+++ vim/install.sh
@@ -4,12 +4,17 @@
 rm -rf backup
 
 
-test -e ~/.vim    && mkdir -p backup && cp -r ~/.vim    backup
-test -e ~/.vimrc  && mkdir -p backup && cp -r ~/.vimrc  backup
+backup () { (test -e $1 && mkdir -p $2 && cp -r $1 $2) }
 
 
-rm -rf ~/.vim   && ln -s $PWD/.vim   ~/.vim
+backup ~/.vim               ./backup
+backup ~/.vimrc             ./backup
+backup ~/.config/powerline  ./backup/.config
+
+
+rm -rf ~/.vim && ln -s $PWD/.vim ~/.vim
 rm -rf ~/.vimrc && ln -s $PWD/.vimrc ~/.vimrc
+rm -rf ~/.config/powerline && ln -s $PWD/.config/powerline ~/.config/powerline
 
 
 test -e backup && mv backup backup-$(date +%s)

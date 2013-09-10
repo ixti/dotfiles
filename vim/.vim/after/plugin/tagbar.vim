53a2baff173d99d6b21bd355c7f36b574d53f068
Aleksey V Zapparov <ixti@member.fsf.org>
2013-09-10 04:41:04 +0200

    [VIM] Support rspec by tagbar

--- vim/.vim/after/plugin/tagbar.vim
+++ vim/.vim/after/plugin/tagbar.vim
@@ -1,2 +1,14 @@
 nmap <silent> <F8> :TagbarToggle<CR>
 imap <silent> <F8> <Esc>:TagbarToggle<CR>
+
+
+let g:tagbar_type_ruby = {
+    \ 'kinds' : [
+        \ 'm:modules',
+        \ 'c:classes',
+        \ 'f:methods',
+        \ 'F:singleton methods',
+        \ 'd:rspec describe',
+        \ 'C:rspec context',
+    \ ],
+\ }
--- vim/install.sh
+++ vim/install.sh
@@ -6,10 +6,12 @@ rm -rf backup
 
 test -e ~/.vim    && mkdir -p backup  && cp -r ~/.vim   backup
 test -e ~/.vimrc  && mkdir -p backup  && cp -r ~/.vimrc backup
+test -e ~/.ctags  && mkdir -p backup  && cp -r ~/.ctags backup
 
 
 rm -rf ~/.vim && ln -s $PWD/.vim ~/.vim
 rm -rf ~/.vimrc && ln -s $PWD/.vimrc ~/.vimrc
+rm -rf ~/.ctags && ln -s $PWD/.ctags ~/.ctags
 
 
 test -e backup && mv backup backup-$(date +%s)

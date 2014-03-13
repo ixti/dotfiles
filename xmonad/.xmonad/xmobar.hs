f30e50d58909fd2457462b14761c70087c7a50ce
Aleksey V Zapparov <ixti@member.fsf.org>
2014-03-13 12:39:08 +0100

    Fix xmobar date/time script runner
    As of xmobar 0.19, `Com` uses safer runInteractiveProcess instead of spawning a
    shell. This patch worksaround this behavior. See:
    
    https://github.com/jaor/xmobar/issues/127
    https://github.com/jaor/xmobar/pull/120

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -8,7 +8,7 @@ Config
   , template      = "<fc=#555>></fc> %kbd% <fc=#555>|</fc> %time% <fc=#555>|</fc> %battery% <fc=#555>|</fc> %StdinReader%"
   , commands      =
     [ Run Kbd [("us", "<fc=#99f>US</fc>"), ("ru", "<fc=#f99>RU</fc>")]
-    , Run Com "~/.xmonad/xmobar/time.sh" [] "time" 600
+    , Run Com "/bin/bash" ["-c", "~/.xmonad/xmobar/time.sh"] "time" 600
     , Run Battery ["-t", "<acstatus> <leftbar>",
                    "--", "-O", "<fc=#9f9>⚡</fc>", "-o", "<fc=#999>⚡</fc>",
                    "-f", "ADP0/online"] 600

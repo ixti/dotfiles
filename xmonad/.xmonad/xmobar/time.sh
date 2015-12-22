29c32482ec759ed98c3fc2f6461a032cb94ebc04
Alexey V Zapparov <ixti@member.fsf.org>
2015-12-23 00:49:41 +0100

    Remove PST time from xmobar

--- xmonad/.xmonad/xmobar/time.sh
+++ xmonad/.xmonad/xmobar/time.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-
 LOCAL_FORMAT="<fc=#999>%_d %b</fc> %H:%M <fc=#999>%Z</fc>"
 EXTRA_FORMAT="<fc=#777>%H:%M</fc> <fc=#555>%Z</fc>"
 
-
 echo -n $(date +"$LOCAL_FORMAT")
 
-
-for TZ in UTC America/New_York America/Los_Angeles; do
+for TZ in UTC America/Los_Angeles; do
   echo -n " <fc=#333>|</fc> $(TZ=$TZ date +"$EXTRA_FORMAT")"
 done

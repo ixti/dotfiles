f808b86f6fb24210185eb5d7034bd78e7f7fcec4
Alexey Zapparov <ixti@member.fsf.org>
2016-03-21 21:37:13 +0100

    Improve xmobar

--- xmonad/.xmonad/xmobar.hs
+++ xmonad/.xmonad/xmobar.hs
@@ -5,13 +5,29 @@ Config
   , lowerOnStart  = True
   , bgColor       = "#000"
   , fgColor       = "#ddd"
-  , template      = "<fc=#555>></fc> %kbd% <fc=#555>|</fc> %time% <fc=#555>|</fc> %battery% <fc=#555>|</fc> %StdinReader%"
+  , template      = "<fc=#555>></fc> %kbd% <fc=#555>|</fc> %time% <fc=#555>|</fc> %coretemp% <fc=#555>|</fc> %battery% <fc=#555>|</fc> %StdinReader%"
   , commands      =
-    [ Run Kbd [("us", "<fc=#99f>US</fc>"), ("ru", "<fc=#f99>RU</fc>")]
-    , Run Com "/bin/bash" ["-c", "~/.xmonad/xmobar/time.sh"] "time" 600
-    , Run Battery ["-t", "<acstatus> <leftbar>",
-                   "--", "-O", "<fc=#9f9>⚡</fc>", "-o", "<fc=#999>⚡</fc>",
-                   "-f", "ADP0/online"] 600
+    [ Run Kbd                 [ ("us" , "<fc=#99f>US</fc>")
+                              , ("ru" , "<fc=#f99>RU</fc>")
+                              ]
+    , Run Battery             [ "--template"  , "<acstatus> <leftbar>"
+                              , "--Low"       , "10" -- units: %
+                              , "--High"      , "80" -- units: %
+                              , "--"
+                              , "-O"          , "<fc=green>+</fc>"
+                              , "-o"          , "<fc=darkred>-</fc>"
+                              , "-i"          , "<fc=darkgreen>=</fc>"
+                              , "-f"          , "ADP0/online"
+                              ] 600
+    , Run CoreTemp            [ "--template" , "<core0><fc=#333>|</fc><core1>"
+                              , "--Low"      , "70" -- units: °C
+                              , "--High"     , "80" -- units: °C
+                              , "--low"      , "#777"
+                              , "--normal"   , "darkorange"
+                              , "--high"     , "darkred"
+                              ] 50
+    , Run Com "/bin/bash"     [ "-c"         , "~/.xmonad/xmobar/time.sh"
+                              ] "time" 600
     , Run StdinReader
     ]
   }

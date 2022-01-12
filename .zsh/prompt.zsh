21e4f64bfc66ec3477d33e9cfd390f154c3bd8a1
Alexey Zapparov <alexey@zapparov.com>
2022-01-12 19:27:36 +0100

    Make spaceship look pure alike :D

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,6 +1,8 @@
 if (( ${+commands[starship]} )); then
+  # https://starship.rs/
   eval "$(starship init zsh)"
 else
+  # https://github.com/sindresorhus/pure/
   export PURE_GIT_PULL=0
   export PURE_PROMPT_SYMBOL="$"
   export PURE_PROMPT_VICMD_SYMBOL=">"

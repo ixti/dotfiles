071dec8eb0c26dd63fcc718446e423cb7c476185
Alexey Zapparov <alexey@zapparov.com>
2022-01-12 17:32:51 +0100

    Use starship when it's available

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,6 +1,10 @@
-export PURE_GIT_PULL=0
-export PURE_PROMPT_SYMBOL="$"
-export PURE_PROMPT_VICMD_SYMBOL=">"
+if (( ${+commands[starship]} )); then
+  eval "$(starship init zsh)"
+else
+  export PURE_GIT_PULL=0
+  export PURE_PROMPT_SYMBOL="$"
+  export PURE_PROMPT_VICMD_SYMBOL=">"
 
-fpath+="${ZDOTDIR:-$HOME}/.zsh/lib/pure"
-source "${ZDOTDIR:-$HOME}/.zsh/lib/pure/pure.plugin.zsh"
+  fpath+="${ZDOTDIR:-$HOME}/.zsh/lib/pure"
+  source "${ZDOTDIR:-$HOME}/.zsh/lib/pure/pure.plugin.zsh"
+fi

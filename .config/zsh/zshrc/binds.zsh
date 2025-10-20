fbdc7b8461b76b55db20d24682769205cc9eeeac
Alexey Zapparov <alexey@zapparov.com>
2025-10-20 17:51:42 +0200

    fix(zsh): Lazy eval vicm key binds

--- .config/zsh/zshrc/binds.zsh
+++ .config/zsh/zshrc/binds.zsh
@@ -1,5 +1,8 @@
 # Key Bindings
 ################################################################################
 
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+# https://github.com/jeffreytse/zsh-vi-mode?tab=readme-ov-file#lazy-keybindings
+function zvm_after_lazy_keybindings() {
+  bindkey -M vicmd 'k' history-substring-search-up
+  bindkey -M vicmd 'j' history-substring-search-down
+}

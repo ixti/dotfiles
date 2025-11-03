d03439d33b80dceb199e2e432dafa4c1beb511da
Alexey Zapparov <alexey@zapparov.com>
2025-11-03 02:47:50 +0100

    fix(nvim): Use external diff sources for lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,10 +2,26 @@ return {
   "nvim-lualine/lualine.nvim",
 
   dependencies = {
+    "lewis6991/gitsigns.nvim",
     "nvim-mini/mini.icons",
   },
 
   config = function()
+    -- Lualine with `diff` causes gitsigns to be force-killed that leaves git index lock:
+    -- * https://github.com/nvim-lualine/lualine.nvim/issues/1096
+    -- ** https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets#using-external-source-for-diff
+    local function diff_source()
+      local gitsigns = vim.b.gitsigns_status_dict
+
+      if gitsigns then
+        return {
+          added    = gitsigns.added,
+          modified = gitsigns.changed,
+          removed  = gitsigns.removed,
+        }
+      end
+    end
+
     require("lualine").setup({
       options = {
         theme        = "auto",
@@ -14,7 +30,10 @@ return {
 
       tabline = {
         lualine_a = { { "tabs", mode = 2 } },
-        lualine_y = { "diff", "diagnostics" },
+        lualine_y = {
+          { "diff", source = diff_source },
+          "diagnostics",
+        },
         lualine_z = { "branch" },
       },
 

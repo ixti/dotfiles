c6077850b172feed58ace6941fcd0d12ab67afe5
Alexey Zapparov <alexey@zapparov.com>
2025-11-15 18:17:51 +0100

    fix(kitty): Decrease font-size

--- .config/kitty/conf.d/font.conf.py
+++ .config/kitty/conf.d/font.conf.py
@@ -4,7 +4,7 @@ from kitty.fonts.common import all_fonts_map
 
 MONO_LISA_VARIABLE = """
 font_family      family="MonoLisa Variable" variable_name=MonoLisaVariableRegular features="ss02 ss03 ss06 ss07 ss08 ss09 ss11 ss16 -calt -liga" wght=180
-font_size        11
+font_size        10
 bold_font        auto
 italic_font      auto
 bold_italic_font auto
@@ -12,10 +12,11 @@ bold_italic_font auto
 
 VICTOR_MONO = """
 font_family       Victor Mono
-font_size         12
+font_size         10
 bold_font         auto
 italic_font       auto
 bold_italic_font  auto
+
 disable_ligatures cursor
 """
 

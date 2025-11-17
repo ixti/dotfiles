898f0e8d3f82e94dc81ec9662683703bda32623f
Alexey Zapparov <alexey@zapparov.com>
2025-11-17 18:35:38 +0100

    fix(nvim): Update snacks config and bindings

--- .config/nvim/lua/plugins/ui/snacks.lua
+++ .config/nvim/lua/plugins/ui/snacks.lua
@@ -11,15 +11,55 @@ return {
 
   opts = {
     indent       = { enabled = true },
-    explorer     = { enabled = true, replace_netrw = true, trash = true },
-    notifier     = { enabled = true, timeout = 3000 },
+    notifier     = { enabled = true, timeout = 1000 },
     statuscolumn = { enabled = true },
+
+    explorer = {
+      replace_netrw = true,
+      trash         = true,
+    },
+
+    styles = {
+      zen = {
+        width = 0.8,
+      },
+    },
   },
 
   keys = {
-    { "<leader>e",  function () Snacks.explorer.reveal() end, desc = "File Explorer" },
-    { "<leader>ff", function() Snacks.picker.smart() end,     desc = "Smart Find Files" },
-    { "<leader>fb", function() Snacks.picker.buffers() end,   desc = "Find buffers" },
-    { "<leader>fg", function() Snacks.picker.grep() end,      desc = "Grep" },
+    { "<leader>e",  function() Snacks.explorer() end,                   desc = "File Explorer (snacks.nvim)" },
+
+    { "<leader>ff", function() Snacks.picker.smart() end,               desc = "Smart Find Files (snacks.nvim)" },
+    { "<leader>fb", function() Snacks.picker.buffers() end,             desc = "Find buffers (snacks.nvim)" },
+    { "<leader>fg", function() Snacks.picker.grep() end,                desc = "Grep (snacks.nvim)" },
+    { "<leader>fr", function() Snacks.picker.recent() end,              desc = "Recent (snacks.nvim)" },
+
+    { "<leader>fc", function() Snacks.picker.command_history() end,     desc = "Command History (snacks.nvim)" },
+    { "<leader>fC", function() Snacks.picker.commands() end,            desc = "Commands (snacks.nvim)" },
+
+    { "<leader>fk", function() Snacks.picker.keymaps() end,             desc = "Keymaps (snacks.nvim)" },
+
+    { "<leader>fd", function() Snacks.picker.diagnostics_buffer() end,  desc = "Buffer Diagnostics (snacks.nvim)" },
+    { "<leader>fD", function() Snacks.picker.diagnostics() end,         desc = "Diagnostics (snacks.nvim)" },
+
+    { "<leader>..", function() Snacks.scratch() end,                    desc = "Toggle Scratch Buffer (snacks.nvim)" },
+    { "<leader>./", function() Snacks.scratch.select() end,             desc = "Select Scratch Buffer (snacks.nvim)" },
+
+    { "<leader>zz", function() Snacks.zen() end,                        desc = "Toggle Scope Focus Mode (snacks.nvim)" },
+
+    -- TODO: Figure out if I'm ever gonan use these
+
+    { "<leader>bd", function() Snacks.bufdelete() end,                  desc = "Delete Buffer (snacks.nvim)" },
+    { "<leader>cR", function() Snacks.rename.rename_file() end,         desc = "Rename File (snacks.nvim)" },
   },
+
+  config = function(_, opts)
+    require("snacks").setup(opts)
+
+    vim.api.nvim_create_user_command(
+      "Notifications",
+      function() Snacks.notifier.show_history() end,
+      {}
+    )
+  end,
 }

d5a90664299e504c4264c5aa99896793e71516ec
Alexey Zapparov <alexey@zapparov.com>
2025-12-03 04:20:51 +0100

    fix(nvim): Explicitly start TS highlighting
    It's not enabled by default

--- .config/nvim/lua/plugins/syntax/treesitter.lua
+++ .config/nvim/lua/plugins/syntax/treesitter.lua
@@ -50,5 +50,11 @@ return {
       "yaml",
       "zsh",
     })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
   end,
 }

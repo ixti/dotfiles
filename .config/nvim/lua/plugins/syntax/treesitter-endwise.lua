a53a28042327396d4feefce2fca1ab74a0607950
Alexey Zapparov <alexey@zapparov.com>
2025-12-05 18:58:43 +0100

    fix(nvim): Switch to forked endwise with fix
    See: https://github.com/RRethy/nvim-treesitter-endwise/pull/57

--- .config/nvim/lua/plugins/syntax/treesitter-endwise.lua
+++ .config/nvim/lua/plugins/syntax/treesitter-endwise.lua
@@ -1,3 +1,8 @@
 return {
-  "RRethy/nvim-treesitter-endwise",
+  -- "RRethy/nvim-treesitter-endwise",
+
+  -- Temporary fix for NeoVim 0.11
+  -- See: https://github.com/RRethy/nvim-treesitter-endwise/pull/57
+  "ixti/nvim-treesitter-endwise",
+  branch = "nvim-0-11-compat",
 }

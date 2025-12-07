a836c67f1ba46197fe4088dea069f17e5113c321
Alexey Zapparov <alexey@zapparov.com>
2025-12-07 19:48:11 +0100

    feat(nvim): Enhance yanking/pasting

---
+++ .config/nvim/lua/plugins/editor/yanky.lua
@@ -0,0 +1,37 @@
+return {
+  "gbprod/yanky.nvim",
+
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "folke/snacks.nvim",
+  },
+
+  opts = {
+    preserve_cursor_position = {
+      enabled = true,
+    },
+
+    ring = {
+      storage = "sqlite",
+    },
+  },
+
+  keys = {
+    {
+      "<leader>p",
+      function()
+        Snacks.picker.yanky()
+      end,
+      mode = { "n", "x" },
+      desc = "Open Yank History (snacks.nvim)",
+    },
+
+    { "y",  "<Plug>(YankyYank)",       mode = { "n", "x" }, desc = "Enhanced yank (yanky)" },
+
+    { "p",  "<Plug>(YankyPutAfter)",   mode = { "n", "x" }, desc = "Put below (yanky)" },
+    { "P",  "<Plug>(YankyPutBefore)",  mode = { "n", "x" }, desc = "Put above (yanky)" },
+
+    { "gp", "<Plug>(YankyGPutAfter)",  mode = { "n", "x" }, desc = "Put below and move cursor (yanky)" },
+    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put above and move cursor (yanky)" },
+  },
+}
